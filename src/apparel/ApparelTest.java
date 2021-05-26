7eddd0227572735dc3741aa7dc5121bb52f2e5d4
surekha128 <surekhahardik1228@gmail.com>
2021-05-26 23:46:49 +0100

    commit

--- src/apparel/ApparelTest.java
+++ src/apparel/ApparelTest.java
@@ -16,10 +16,10 @@ public class ApparelTest extends BaseTest {
         openBrowser(baseurl);
     }
 
-      @After
+    /*  @After
       public void closebrowser(){
           driver.quit();
-      }
+      }*/
 
 //1. click on apparel and navigate to clothing
     @Test
@@ -36,7 +36,9 @@ public class ApparelTest extends BaseTest {
     public void verifyThatTheProductAddedSuccessfullyAndPlaceOrderSuccessfully() throws InterruptedException {
        Thread.sleep(10000);
         mouseHoverToElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[3]/a[1]"));
+      //  mouseHoverToElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[3]/ul[1]/li[2]/a[1]"));
         mouseHoverToElement(By.xpath("//body/div[6]/div[2]/ul[1]/li[3]/ul[1]/li[2]/a[1]"));
+
 
 //  click on "Grid"
         Thread.sleep(5000);
@@ -76,18 +78,26 @@ public class ApparelTest extends BaseTest {
         String expectedMessage4 = "Shopping cart";
         String actualMessage4 = gettextfromelemrnt(By.xpath("//h1[contains(text(),'Shopping cart')]"));
         Assert.assertEquals("cannot show bar notification", expectedMessage4, actualMessage4);
-
-//  Verify the price “$15.00”
+//        quantity "5"
         Thread.sleep(2000);
-        String expectedMessage5 = "$15.00";
-        String actualMessage5 = gettextfromelemrnt(By.xpath("//tbody/tr[4]/td[2]"));
+        driver.findElement(By.xpath("//input[@value='1']")).clear();
+        sendtexttoelement(By.xpath("//input[@value='1']"), "5");
+        clickonelement(By.xpath("//button[contains(text(),'Update shopping cart')]"));
+
+
+//  Verify the price “$75.00”
+        Thread.sleep(2000);
+        String expectedMessage5 = "$75.00";
+        String actualMessage5 = gettextfromelemrnt(By.xpath("//body[1]/div[6]/div[3]/div[1]/div[1]/div[1]/div[2]/div[1]/form[1]/div[3]/div[2]/div[1]/table[1]/tbody[1]/tr[4]/td[2]/span[1]/strong[1]"));
         Assert.assertEquals("cannot verify total", expectedMessage5, actualMessage5);
 
 
 //  click on checkbox “I agree with the terms of service”
+        Thread.sleep(1000);
         clickonelement(By.xpath("//input[@id='termsofservice']"));
 
 //  Click on “CHECKOUT”
+        Thread.sleep(1000);
         clickonelement(By.xpath("//button[@id='checkout']"));
 
 //  Verify the Text “Welcome, Please Sign In!”
@@ -131,7 +141,7 @@ clickonelement(By.xpath("//button[contains(text(),'Register')]"));
         selectbyvaluefromdropdown(By.xpath("//select[@name='DateOfBirthYear']"),"2000");
 
         //EMAIL CHANGE EVERYTIME
-        sendtexttoelement(By.xpath("//input[@id='Email']"),"hur6h2@yahoo.com");
+        sendtexttoelement(By.xpath("//input[@id='Email']"),"vot@yahoo.com");
         sendtexttoelement(By.xpath("//input[@id='Company']"),"amazon");
         sendtexttoelement(By.xpath("//input[@id='Password']"),"send962d");
         sendtexttoelement(By.xpath("//input[@id='ConfirmPassword']"),"send962d");
@@ -210,104 +220,6 @@ Thread.sleep(5000);
         String expectedMessage14 = "Welcome to our store";
         String actualMessage14 = gettextfromelemrnt(By.xpath("//h2[contains(text(),'Welcome to our store')]"));
         Assert.assertEquals("order not placed ", expectedMessage14, actualMessage14);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   Click on “Returning Customer” tab and fill the mandatory fields
-       // sendtexttoelement(By.xpath("//input[@id='Email']"),"motu@yahoo.com");
-       // sendtexttoelement(By.xpath("//input[@id='Password']"),"qwe123");
-
-//  click on check box
-     //   clickonelement(By.xpath("//input[@id='RememberMe']"));
-
-//click on login button
-       // clickonelement(By.xpath("//button[contains(text(),'Log in')]"));
-//click on "Register"
-     /*   clickonelement(By.xpath("//button[contains(text(),'Register')]"));
-// verify the "Register"
-
-        Thread.sleep(2000);
-        String expectedMessage7 = "Register";
-        String actualMessage7 = gettextfromelemrnt(By.xpath("//h1[contains(text(),'Register')] "));
-        Assert.assertEquals("not on Register", expectedMessage7, actualMessage7);
-
-//        fill the mandatory fields
-        Thread.sleep(5000);
-        clickonelement(By.xpath("//input[@id='gender-male']"));
-        sendtexttoelement(By.xpath("//input[@id='FirstName']"),"aline");
-        sendtexttoelement(By.xpath("//input[@id='LastName']"),"cat");
-        selectbyvaluefromdropdown(By.xpath("//body/div[6]/div[3]/div[1]/div[1]/div[1]/div[2]/form[1]/div[1]/div[2]/div[4]/div[1]/select[1]"),"20");
-        selectbyvaluefromdropdown(By.xpath("//body/div[6]/div[3]/div[1]/div[1]/div[1]/div[2]/form[1]/div[1]/div[2]/div[4]/div[1]/select[2]"),"12");
-        selectbyvaluefromdropdown(By.xpath("//body/div[6]/div[3]/div[1]/div[1]/div[1]/div[2]/form[1]/div[1]/div[2]/div[4]/div[1]/select[3]"),"1942");
-//email
-        sendtexttoelement(By.xpath("//input[@id='Email']"),"Seend1962@dayrep.com");
-
-        sendtexttoelement(By.xpath("//input[@id='Company']"),"amazon");
-//pass
-        sendtexttoelement(By.xpath("//input[@id='Password']"),"send962d");
-        sendtexttoelement(By.xpath("//input[@id='ConfirmPassword']"),"send962d");
-        clickonelement(By.xpath("//button[@id='register-button']"));
-
-
-//        verify "Your registration completed"
-        Thread.sleep(2000);
-        String expectedMessage8 = "Your registration completed";
-        String actualMessage8 = gettextfromelemrnt(By.xpath("//div[contains(text(),'Your registration completed')]"));
-        Assert.assertEquals("not on Register", expectedMessage8, actualMessage8);
-
-//        click on countinue
-        clickonelement(By.xpath("//a[contains(text(),'Continue')]"));
-
-
-//      click  checkbox
-        Thread.sleep(1000);
-        clickonelement(By.xpath("//input[@id='termsofservice']"));
-
-//        click on checkout
-        Thread.sleep(1000);
-        clickonelement(By.xpath("//button[@id='checkout']"));
-
-//         Fill the Mandatory fields
-        Thread.sleep(5000);
-        sendtexttoelement(By.xpath("//select[@id='BillingNewAddress_CountryId']"),"Bhutan");
-       sendtexttoelement(By.xpath("//input[@id='BillingNewAddress_City']"),"brighton");
-       sendtexttoelement(By.xpath("//input[@id='BillingNewAddress_Address1']"),"po1 qa2");
-        sendtexttoelement(By.xpath("//input[@id='BillingNewAddress_ZipPostalCode']"),"309091");
-        sendtexttoelement(By.xpath("//input[@id='BillingNewAddress_PhoneNumber']"),"07588771112");
-
-
-//         Click on “CONTINUE”
-        Thread.sleep(3000);
-        clickonelement(By.xpath("//body/div[6]/div[3]/div[1]/div[1]/div[1]/div[2]/ol[1]/li[1]/div[2]/div[1]/button[4]"));
-
-*/
-
-
-
-
 
 
 
